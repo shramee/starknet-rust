c39e5c7815ec94eea109434231f26e1edca188b1
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-11 04:53:11 +0800

    Some starknet properties

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,47 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn logAny(s: JsValue);
+
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn id() -> Result<String, JsValue>;
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn name() -> Result<String, JsValue>;
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn version() -> Result<String, JsValue>;
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn icon() -> Result<String, JsValue>;
+
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn selectedAddress() -> Result<String, JsValue>;
+
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn chainId() -> Result<String, JsValue>;
+
+    #[wasm_bindgen(js_namespace = starknet, catch)]
+    pub fn enable() -> Result<String, JsValue>;
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, starknet-wasm!");
+pub fn greet() {}
+
+#[wasm_bindgen(start)]
+pub fn run() {
+    let msg = name();
+    match msg {
+        Ok(val) => log(&val),
+        Err(_err) => logAny(_err),
+    }
+    let msg = version();
+    match msg {
+        Ok(val) => log(&val),
+        Err(_err) => logAny(_err),
+    }
 }
