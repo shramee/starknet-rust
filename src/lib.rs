151818af564ed1be4debcb33681345f0be02697b
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-12 04:51:35 +0800

    Starknet type

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-mod utils;
-
-use futures::executor::block_on;
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::JsFuture;
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -11,47 +7,54 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    # [wasm_bindgen (js_name = starknet)]
+    pub static STARKNET: StarknetObj;
 
+    #[wasm_bindgen]
+    #[derive(Debug, Clone)]
+    pub type StarknetObj;
+
+    # [wasm_bindgen (structural , method , getter , js_namespace = starknet , js_name = isConnected)]
+    pub fn is_connected(this: &StarknetObj) -> bool;
+
+    #[wasm_bindgen(structural , method , js_namespace = starknet)]
+    pub async fn enable(this: &StarknetObj) -> JsValue;
+}
+
+#[wasm_bindgen]
+extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 
     #[wasm_bindgen(js_namespace = console, js_name = log)]
-    fn logAny(s: JsValue);
-
-    #[wasm_bindgen(js_namespace = starknet)]
-    pub static id: String;
-    #[wasm_bindgen(js_namespace = starknet)]
-    pub static name: String;
-    #[wasm_bindgen(js_namespace = starknet)]
-    pub static version: String;
-    #[wasm_bindgen(js_namespace = starknet)]
-    pub static icon: String;
-
-    #[wasm_bindgen(js_namespace = starknet, catch)]
-    pub fn selectedAddress() -> Result<String, JsValue>;
-
-    #[wasm_bindgen(js_namespace = starknet, catch)]
-    pub fn chainId() -> Result<String, JsValue>;
-
-    #[wasm_bindgen(js_namespace = starknet,)]
-    async fn enable() -> JsValue;
-
-    #[wasm_bindgen(js_namespace = starknet, js_name=isPreauthorized, catch)]
-    async fn is_preauthorised() -> Result<JsValue, JsValue>;
+    fn logAny(s: bool);
 }
 
 #[wasm_bindgen]
-pub fn greet() {}
+pub fn greet() {
+    if STARKNET.is_connected() {
+        log("Connected!");
+    } else {
+        log("Not connected");
+    }
+}
 
 #[wasm_bindgen(start)]
 pub async fn run() {
-    let preauth = is_preauthorised().await;
-    let _status = enable().await;
+    log("Connecting...");
+    if STARKNET.is_connected() {
+        log("Connected!");
+    } else {
+        log("Not connected");
+    }
+    connect().await;
+    if STARKNET.is_connected() {
+        log("Connected!");
+    } else {
+        log("Not connected");
+    }
+}
 
-    logAny(_status)
-    // match preauth {
-    //     Ok(val) => logAny(val),
-    //     Err(e) => logAny(e),
-    // }
+pub async fn connect() {
+    STARKNET.enable().await;
 }
